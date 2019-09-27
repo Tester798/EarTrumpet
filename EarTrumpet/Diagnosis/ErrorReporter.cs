edfd43abf5aa61f0336f55d2bf1caba0a9eb1ef6
Tester798 <Tester798@users.noreply.github.com>
2019-09-27 03:16:14 +0300

    feature-remove-bugsnag

--- EarTrumpet/Diagnosis/ErrorReporter.cs
+++ EarTrumpet/Diagnosis/ErrorReporter.cs
@@ -1,6 +1,4 @@
-﻿using Bugsnag;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 
 namespace EarTrumpet.Diagnosis
@@ -8,7 +6,6 @@ namespace EarTrumpet.Diagnosis
     class ErrorReporter
     {
         private static ErrorReporter s_instance;
-        private readonly Client _bugsnagClient;
         private readonly CircularBufferTraceListener _listener;
         private readonly AppSettings _settings;
 
@@ -21,19 +18,6 @@ namespace EarTrumpet.Diagnosis
             _settings = settings;
             Trace.Listeners.Clear();
             Trace.Listeners.Add(_listener);
-
-            if (_settings.IsTelemetryEnabled)
-            {
-                try
-                {
-                    _bugsnagClient = new Client(Bugsnag.ConfigurationSection.Configuration.Settings);
-                    _bugsnagClient.BeforeNotify(new Middleware(OnBeforeNotify));
-                }
-                catch (Exception ex)
-                {
-                    Trace.WriteLine($"ErrorReporter .ctor Failed: {ex}");
-                }
-            }
         }
 
         public void DisplayDiagnosticData()
@@ -45,35 +29,6 @@ namespace EarTrumpet.Diagnosis
         private void LogWarningInstance(Exception ex)
         {
             Trace.WriteLine($"## Warning Notify ##: {ex}");
-            _bugsnagClient?.Notify(ex, Severity.Warning);
-        }
-
-        private void OnBeforeNotify(Bugsnag.Payload.Report error)
-        {
-            try
-            {
-                // Remove default properties that we don't need.
-                error.Event.Device.Clear();
-                Fill(error.Event.Device, SnapshotData.Device);
-
-                Fill(error.Event.App, SnapshotData.App);
-
-                var appSettings = new Dictionary<string, object>();
-                error.Event.Metadata.Add("AppSettings", appSettings);
-                Fill(appSettings, SnapshotData.AppSettings);
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine($"## ErrorReporter OnBeforeNotify: {ex}");
-            }
-        }
-
-        private void Fill(Dictionary<string, object> dest, Dictionary<string, Func<object>> source)
-        {
-            foreach (var key in source.Keys)
-            {
-                dest[key] = SnapshotData.InvokeNoThrow(source[key]);
-            }
         }
     }
 }
